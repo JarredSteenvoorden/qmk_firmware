01d1563dc643558c27f56b349e9668a67b039219
Jarred Steenvoorden <jarred.steenvoorden@fetechgroup.com>
2021-06-06 16:11:41 +1000

    Move backspace onto combo and quote to base layer

--- keyboards/underscore33/rev2/keymaps/jarred/keymap.c
+++ keyboards/underscore33/rev2/keymaps/jarred/keymap.c
@@ -4,7 +4,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT_33_big_space(
     KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   ,
-    NAV_A  , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_BSPC,
+    NAV_A  , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_QUOT,
     LSFT_Z , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , RSF_SLH,
              KC_ENT ,                        NUM_SPC,                       KC_ENT
   ),
@@ -12,14 +12,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LW] = LAYOUT_33_big_space(
     KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   ,
     KC_F12 , KC_GRV , KC_EQL , KC_MINS, KC_F10 , KC_F11 , KC_SCLN, KC_BSLS, KC_LBRC, KC_RBRC,
-    _______, KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , _______,
+    KC_LSFT, KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_RSFT,
              RESET  ,                        _______,                       _______
   ),
 
   [_NV] = LAYOUT_33_big_space(
     _______, _______, KC_LGUI,  KC_DEL, KC_BSPC, _______, KC_HOME, KC_UP  , KC_END ,  KC_DEL,
     _______, _______, KC_LSFT, KC_LCTL,  KC_ENT, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC,
-    _______, _______, _______, ALT_TAB, _______, KC_PGUP, KC_PGDN, _______, _______, _______,
+    _______, _______, CTL_TAB, ALT_TAB, _______, _______, KC_PGUP, KC_PGDN, _______, _______,
              _______,                        _______,                       _______
   ),
 };
